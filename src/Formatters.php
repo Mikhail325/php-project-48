c85871f9621618f734c8e71bdefe80e2f3257196
Mikail <miklsuhanov@gmail.com>
2023-05-10 13:41:14 +1000

    change stan

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,8 +6,8 @@ function formatSelection(array $data, string $format): string
 {
     $corectFormate = ['stylish', 'plain', 'json'];
     $render = "Differ\Formatters" . '\\' . $format . '\\' . 'render';
-    
-    if (in_array($format, $corectFormate, true)) {    
+
+    if (in_array($format, $corectFormate, true)) {
         return $render($data);
     }
 
