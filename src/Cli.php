5a6595e3db6090047d0de12f19ca991de83affdb
Mikail <miklsuhanov@gmail.com>
2023-05-10 12:42:56 +1000

    change genDiff

---
+++ src/Cli.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Differ\Cli;
+
+use function Differ\Differ\genDiff;
+
+const DOC = <<<DOC
+gendiff -h
+
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
+DOC;
+
+function run()
+{
+    $args = \Docopt::handle(DOC, array('version' => 'GenDiff 1.0'));
+    $diff = genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
+    print_r($diff);
+}
