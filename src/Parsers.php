def440cc12c180ad47fea800f42d3ef43c9b93ad
Mikail <miklsuhanov@gmail.com>
2023-05-24 10:59:08 +1000

    change Parsers.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,12 +7,12 @@ use Symfony\Component\Yaml\Yaml;
 function parset(mixed $data, string $expation): array
 {
     switch ($expation) {
-        case ('json'):
+        case 'json':
             return json_decode($data, true);
-        case ('yaml'):
-        case ('yml'):
+        case 'yaml':
+        case 'yml':
             return Yaml::parse($data);
         default:
-            throw new \Exception("Unknown file format {$expation}");
+            throw new \Exception("Unknown file format $expation");
     }
 }
